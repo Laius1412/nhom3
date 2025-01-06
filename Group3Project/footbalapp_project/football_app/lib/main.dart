64d563e858f0ceb5c1cf31464fbdbb2b12a05245
vanhlion <vietanhnguyen32004@gmail.com>
2025-01-06 13:27:37 +0700

    Giao diện

--- Group3Project/footbalapp_project/football_app/lib/main.dart
+++ Group3Project/footbalapp_project/football_app/lib/main.dart
@@ -1,125 +1,91 @@
 import 'package:flutter/material.dart';
-
+import 'package:projectmobile/screen/home_screen.dart';
+import 'package:projectmobile/screen/math_screen.dart';
+import 'package:projectmobile/screen/news_screen.dart';
+import 'package:projectmobile/screen/profile_screen.dart';
+import 'package:projectmobile/screen/stats_screen.dart';
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color.fromARGB(255, 0, 60, 0), // Màu xanh đậm hơn
+          primary: const Color.fromARGB(255, 0, 50, 0), // Màu chính đậm hơn
+          secondary: Colors.green, // Màu phụ là xanh lá
+        ),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'FLASH SOCCER'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
+  int _selectedIndex = 2;
+  final List<Widget> _screens = <Widget>[
+    NewsScreen(),
+    MathScreen(),
+    HomeScreen(),
+    StatsScreen(),
+    ProfileScreen(),
+  ];
+  void _onItemTapped(int index) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _selectedIndex = index; // Cập nhật chỉ số mục được chọn
     });
   }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: _screens[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: Colors.black, // Màu nền cho bottom bar
+          type: BottomNavigationBarType.fixed, // Cho phép hiển thị tối đa 5 mục
+          selectedItemColor: Colors.white, // Màu mục được chọn
+          unselectedItemColor: Colors.green, // Màu mục chưa chọn
+          currentIndex: _selectedIndex, // Vị trí mục đang được chọn
+          onTap: _onItemTapped,
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.assignment), // Icon cho menu Tin tức
+              label: "Tin tức",
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            BottomNavigationBarItem(
+              icon: Icon(Icons.sports_soccer), // Icon cho menu Trận đấu
+              label: "Trận đấu",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home), // Icon cho menu Trang chủ
+              label: "Trang chủ",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.equalizer), // Icon cho menu Search
+              label: "Thống kê",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person), // Icon cho menu chính
+              label: "Cá nhân",
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
---
+++ Group3Project/footbalapp_project/football_app/lib/screen/profile_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ProfileScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Cá nhân'),
+      ),
+      body: Center(
+        child: Text('Cá nhân'),
+      ),
+    );
+  }
+}
