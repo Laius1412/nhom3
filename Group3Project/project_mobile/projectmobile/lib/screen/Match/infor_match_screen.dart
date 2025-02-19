f3d636e93439f14de32df5bd54bf55d185cbbeba
Laius1412 <118721682+Laius1412@users.noreply.github.com>
2025-02-19 11:21:56 +0700

    update informatch screen frontend

--- Group3Project/project_mobile/projectmobile/lib/screen/Match/infor_match_screen.dart
+++ Group3Project/project_mobile/projectmobile/lib/screen/Match/infor_match_screen.dart
@@ -4,15 +4,34 @@ class InforMatchScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(
-        body: Container(
-          color: Color(0xFF000000),
-          child: Center(
-            child: Text(
-              "This is information about this match",
+      child: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Color(0xFF1E1E1E),
+            iconTheme: IconThemeData(color: Colors.white),
+            title: Text(
+              "Thống kê trận đấu",
               style: TextStyle(color: Colors.white),
             ),
+            bottom: TabBar(
+              tabs: [
+                Tab(
+                  text: "Overview",
+                ),
+                Tab(
+                  text: "Line-up",
+                ),
+                Tab(
+                  text: "Stats",
+                ),
+              ],
+              labelColor: Colors.green[800],
+              indicatorColor: Colors.green[800],
+              unselectedLabelColor: Colors.white,
+            ),
           ),
+          body: TabBarView(children: []),
         ),
       ),
     );
