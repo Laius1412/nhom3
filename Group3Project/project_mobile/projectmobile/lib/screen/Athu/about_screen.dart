38a8d37ea8a97315695bc7d458d8b3273cc6d497
QuangGiap73 <voquanggiap73@gmail.com>
2025-02-23 21:17:03 +0700

    upload điều khoản dịch vụ

--- Group3Project/project_mobile/projectmobile/lib/screen/Athu/about_screen.dart
+++ Group3Project/project_mobile/projectmobile/lib/screen/Athu/about_screen.dart
@@ -7,8 +7,20 @@ class AboutScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Giới thiệu"),
-        backgroundColor: Colors.blueAccent,
+        title: Text(
+           "Giới thiệu",
+          style: TextStyle(color: Colors.white),
+        ),
+        iconTheme: IconThemeData(color: Colors.white), // Đặt màu nút back thành trắng
+        flexibleSpace: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Color.fromARGB(255, 0, 0, 0), Color.fromARGB(255, 77, 16, 28)],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+        ),
       ),
       body: const Padding(
         padding: EdgeInsets.all(16.0),
